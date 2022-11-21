6ba4a5a60e79fa8bb6fd1b8e114a0fa358b02b5d
Joseph Strauss <jstrauss16@proton.me>
2022-11-21 10:46:42 -0600

    Add submission form

--- src/main/java/todayilearned/web/SubmissionController.java
+++ src/main/java/todayilearned/web/SubmissionController.java
@@ -3,6 +3,7 @@ package todayilearned.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import todayilearned.Submission;
 import todayilearned.data.SubmissionRepository;
@@ -19,12 +20,24 @@ public class SubmissionController {
     }
 
     @GetMapping
-    public void homePage() {
+    public String homePage() {
         Iterable<Submission> submissions = submissionRepo.findAll();
         for (Submission submission : submissions) {
             log.info("Submission: {}", submission);
             System.out.println(submission);
         }
+        return "home";
+    }
+
+    @GetMapping("/submit")
+    public String submissionForm() {
+        return "submissionForm";
+    }
+
+    @PostMapping("/submit")
+    public String processSubmission(Submission submission) {
+        submissionRepo.save(submission);
+        return "redirect:/";
     }
 
 
