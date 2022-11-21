90f540d4f61e2cb1376812ab4dc2f120838dbe2e
Joseph Strauss <jstrauss16@proton.me>
2022-11-21 14:47:39 -0600

    Add home page that displays a list of submissions

--- src/main/java/todayilearned/web/SubmissionController.java
+++ src/main/java/todayilearned/web/SubmissionController.java
@@ -2,11 +2,16 @@ package todayilearned.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import todayilearned.Submission;
 import todayilearned.data.SubmissionRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -38,6 +43,14 @@ public class SubmissionController {
     public String processSubmission(Submission submission) {
         submissionRepo.save(submission);
         return "redirect:/";
+    }
+
+    @ModelAttribute(name = "submissions")
+    public List<Submission> addSubmissionsToModel(Model model) {
+        List<Submission> submissions = new ArrayList<>();
+        submissionRepo.findAll().forEach(i -> submissions.add(i));
+        return submissions;
+
     }
 
 
