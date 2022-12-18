609b97de0ac8353af85f27e81a67d2a737518537
Joe Strauss <cuppajava@tutanota.com>
2022-12-18 15:43:34 -0600

    Begin work on edit feature. See TODO

--- src/main/java/todayilearned/web/SubmissionFormController.java
+++ src/main/java/todayilearned/web/SubmissionFormController.java
@@ -4,16 +4,14 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import todayilearned.Submission;
 import todayilearned.User;
 import todayilearned.data.SubmissionRepository;
 import todayilearned.util.HtmlService;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/submit")
@@ -28,11 +26,19 @@ public class SubmissionFormController {
     }
 
     @GetMapping()
-    public String submissionForm(Model model) {
+    public String submissionForm(@RequestParam(required = false, name = "edit")  Long submissionId, Model model) {
+        if (submissionId != null) {
+            Optional<Submission> submission = submissionRepo.findById(submissionId);
+            if (submission.isPresent()) {
+                model.addAttribute("submission", submission.get());
+                return "submissionForm";
+            }
+        }
         model.addAttribute("submission", new Submission());
         return "submissionForm";
     }
 
+    // TODO: if an edit parameter is posted and is a valid id, update post instead of making a new one.
     @PostMapping()
     public String processSubmission(@ModelAttribute @Valid Submission submission, Errors errors, @AuthenticationPrincipal User author) {
         if (errors.hasErrors())
